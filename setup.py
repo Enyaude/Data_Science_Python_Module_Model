3c4290b18ec8ced2bccb10c2e8d41f74696aae0d
Samuel-Enyaude <enyaudesamuel@hotmail.com>
2024-08-19 15:16:53 +0100

    First commit in DS format

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Python_Module_for_Data_Analysis',
+    name='Python_Module_for_Data_Science',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
-    description='EDSA: Python module for data Analysis package',
+    description='EDSA: Python module for data science package',
     long_description=open('README.md').read(),
     install_requires=['numpy', 'pandas', 'scipy'],
-    url='https://github.com/Enyaude/Python_Module_for_Data_Analysis.git',
+    url='https://github.com/Enyaude/Python_Module_for_Data_Science.git',
     author='<Enyaude>',
     author_email='<enyaudesamuel@hotmail.com>'
     )
